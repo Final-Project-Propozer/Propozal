e918a7c85405f4d10662857f1eb1873cc9adcdb3
윤혜진 <lold2424@gmail.com>
2025-07-30 17:50:16 +0900

    chore: 백엔드 기본 설정

---
+++ propozal-backend/src/main/java/com/propozal/backend/PropozalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.propozal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropozalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropozalBackendApplication.class, args);
+	}
+
+}
